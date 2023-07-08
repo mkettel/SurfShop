c7e24212df389d5912acb79e2265d4bf81acaa8e
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-07-08 18:28:44 -0400

    got rid of some imports

--- starter/src/Surfboard.jsx
+++ starter/src/Surfboard.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useRef } from "react";
 import { MeshTransmissionMaterial, useGLTF, CameraControls } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useControls } from 'leva'
-import { Vector3 } from "three";
 import * as THREE from 'three'
 import { useRoute, useLocation } from 'wouter'
 
