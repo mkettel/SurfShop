aed99f91853d35bb3faaf35a93bf7df10488a32c
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-08-16 17:11:22 -0400

    camera controls added. will push this code and then make the flip code

--- starter/src/Butterstick.jsx
+++ starter/src/Butterstick.jsx
@@ -1,6 +1,6 @@
 
 import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, CameraControls } from "@react-three/drei";
 import { useControls } from 'leva'
 
 
@@ -33,8 +33,14 @@ export function Butterstick(props) {
   bottomMaterial.color.set(bottomsColor);
   // #EBB4D5
 
+  // Refs
+  const butterstick = useRef(); // access to the surfboard
+  const controlsRef = useRef(); // access to the camera controls
+
   return (
-    <group {...props} dispose={null}>
+    <>
+    <CameraControls ref={controlsRef} minPolarAngle={.3} maxPolarAngle={Math.PI / 2.3} minAzimuthAngle={-.7} maxAzimuthAngle={1} minDistance={3} maxDistance={11}  />
+    <group {...props} dispose={null} ref={butterstick}>
       <mesh
         castShadow
         // receiveShadow
@@ -43,7 +49,7 @@ export function Butterstick(props) {
         material={materials.glossy}
         position={[0.039, 1.216, -0.292]}
         scale={[1, 1, 1.123]}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -52,7 +58,7 @@ export function Butterstick(props) {
         material={topMaterial}
         position={[0.039, 1.216, -0.292]}
         scale={[1, 1, 1.123]}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -61,7 +67,7 @@ export function Butterstick(props) {
         material-color={bottomsColor}
         position={[0.039, 1.216, -0.292]}
         scale={[1, 1, 1.123]}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -70,7 +76,7 @@ export function Butterstick(props) {
         position={[0.653, 0.077, -2.872]}
         rotation={[-3.084, 0, -Math.PI]}
         scale={[0.142, 0.366, 0.441]}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -79,7 +85,7 @@ export function Butterstick(props) {
         position={[-0.477, 0.077, -2.872]}
         rotation={[-3.084, 0, -Math.PI]}
         scale={[0.142, 0.366, 0.441]}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -88,7 +94,7 @@ export function Butterstick(props) {
         position={[0.028, 0.682, -0.006]}
         rotation={[-Math.PI, 0, -Math.PI]}
         scale={[0.016, 1, 3.522]}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -106,7 +112,7 @@ export function Butterstick(props) {
         position={[0.077, 0.651, 0.756]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.128}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -115,7 +121,7 @@ export function Butterstick(props) {
         position={[0.067, 0.65, 0.613]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.147}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -124,7 +130,7 @@ export function Butterstick(props) {
         position={[0.067, 0.645, 0.473]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.147}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -133,7 +139,7 @@ export function Butterstick(props) {
         position={[0.067, 0.645, 0.336]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.147}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -142,7 +148,7 @@ export function Butterstick(props) {
         position={[0.077, 0.645, 0.22]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.128}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -151,7 +157,7 @@ export function Butterstick(props) {
         position={[0.067, 0.643, 0.091]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.147}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -160,7 +166,7 @@ export function Butterstick(props) {
         position={[0.075, 0.636, -0.024]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.116}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -169,7 +175,7 @@ export function Butterstick(props) {
         position={[0.065, 0.636, -0.134]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.116}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -178,7 +184,7 @@ export function Butterstick(props) {
         position={[0.067, 0.634, -0.594]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.147}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -187,7 +193,7 @@ export function Butterstick(props) {
         position={[0.076, 0.635, -0.711]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.127}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -196,7 +202,7 @@ export function Butterstick(props) {
         position={[0.073, 0.629, -0.843]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.137}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -205,7 +211,7 @@ export function Butterstick(props) {
         position={[0.064, 0.629, -0.977]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.137}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -214,7 +220,7 @@ export function Butterstick(props) {
         position={[0.067, 0.629, -1.121]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.147}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -223,7 +229,7 @@ export function Butterstick(props) {
         position={[0.067, 0.629, -1.255]}
         rotation={[Math.PI, 0, Math.PI]}
         scale={0.147}
-      />
+        />
       <mesh
         castShadow
         // receiveShadow
@@ -232,8 +238,9 @@ export function Butterstick(props) {
         position={[0.024, 0.64, -0.305]}
         rotation={[3.135, 0.078, 0.049]}
         scale={0.31}
-      />
+        />
     </group>
+  </>
   );
 }
 
